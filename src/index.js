a67d959ebc41373cdf48242b1d78410b3a595e40
scps940327 <scps940327@gamil.com>
2018-11-22 17:19:58 +0800

    20181122新增sass編譯部分
    - npm install sass-loader node-sass webpack --save-dev
    - npm install sass-loader node-sass --save-dev
    可以使SASS經過編譯呈現在HTML上

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import FormContainer from "./js/components/container/FormContainer";
+// import style from './scss/style.scss';
+
+require('./scss/style.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   module: {
     rules: [
@@ -14,6 +15,25 @@ module.exports = {
         use: [
           {
             loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000
+            }
           }
         ]
       }
